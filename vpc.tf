d150173efb431e4675dea33169df0e6a31cdc4f1
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-11-04 13:56:57 +0530

    vpc-module

---
+++ vpc.tf
@@ -0,0 +1,80 @@
+#creating vpc roboshop-dev
+
+resource "aws_vpc" "main" {
+  cidr_block       = var.cidr_block
+  instance_tenancy = "default"
+
+  enable_dns_hostnames = "true"
+
+  tags = merge (
+    var.vpc_tags,
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}"
+    }
+  )
+}
+
+#creating IGW  roboshop-dev
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.igw_tags,
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}"
+    }
+  )
+}
+
+#create public subnets
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+
+  availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true                    #as it is private subent we need to write this
+
+  tags = merge(
+    var.public_subnet_tags,
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+    }
+  )
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+    var.private_subnet_tags,
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
+    }
+  )
+}
+
+resource "aws_subnet" "database" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidrs[count.index]
+
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+    var.database_subnet_tags,
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-database-${local.az_names[count.index]}"
+    }
+  )
+}
